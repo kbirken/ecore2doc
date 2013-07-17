12f9f5d62297ed5d02ac2c9a1167af3e0231f3fa
Klaus Birken <klaus.birken@gmail.com>
2013-07-17 15:48:04 +0200

    Removed warnings.

--- plugins/src/org/nanosite/ecore2doc/franca/GenXdocForFrancaAPI.java
+++ plugins/src/org/nanosite/ecore2doc/franca/GenXdocForFrancaAPI.java
@@ -1,20 +1,8 @@
 package org.nanosite.ecore2doc.franca;
 
-import static org.junit.Assert.*;
-
-import java.io.IOException;
-import java.util.Map;
-
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.codegen.ecore.genmodel.GenClass;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
-import org.eclipse.emf.codegen.ecore.genmodel.GenModelPackage;
-import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.util.Modules2;
 import org.franca.core.dsl.FrancaIDLRuntimeModule;
 import org.franca.core.franca.FrancaPackage;
@@ -23,7 +11,6 @@ import org.nanosite.ecore2doc.Ecore2DocGenerator;
 import org.nanosite.ecore2doc.GenModelLoader;
 
 import com.google.inject.Guice;
-import com.google.inject.Inject;
 import com.google.inject.Injector;
 
 public class GenXdocForFrancaAPI {
@@ -32,7 +19,7 @@ public class GenXdocForFrancaAPI {
 			
     @Test
 	public void test() {
-        EcorePlugin.getEPackageNsURIToGenModelLocationMap().put(FrancaPackage.eNS_URI,
+        EcorePlugin.getEPackageNsURIToGenModelLocationMap(false).put(FrancaPackage.eNS_URI,
         	URI.createURI(myFrancaSources + "/plugins/org.franca.core/model/Franca.genmodel"));
         
     	Injector injector = Guice.createInjector(Modules2.mixin(new FrancaIDLRuntimeModule(), new FrancaAPI2XdocModule()));
